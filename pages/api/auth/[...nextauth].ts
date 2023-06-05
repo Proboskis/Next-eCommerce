a83644b842b971e7b0f2138bc0b64b5cea58c389
Proboskis <nikolasurla22@gmail.com>
2023-06-05 11:19:07 +0200

    2. Setting up next-auth, sign in with Google, fixing up the prisma migrations due to some errors regarding the new dependencies releases.

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+// noinspection TypeScriptCheckImport,TypeScriptValidateTypes
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+});
