9aa1305f97df44eb8fde87b0cdda0b5ff31df17a
Proboskis <nikolasurla22@gmail.com>
2023-07-05 14:07:03 +0200

    4. Adding stripe to the project, creating the stripe user, updating the user moder with stripeCustomerId, etc.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
@@ -15,4 +16,23 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    events: {
+        createUser: async ({user}) => {
+            const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+                apiVersion: process.env.STRIPE_API_VERSION
+            });
+            // let us create a stripe customer
+            if(user.name && user.email) {
+                const customer = await stripe.customers.create({
+                    email: user.email,
+                    name: user.name,
+                });
+                // also update our prisma user with the stripecustomerid
+                await prisma.user.update({
+                    where: {id: user.id},
+                    data: {stripeCustomerId: customer.id}
+                })
+            }
+        }
+    }
 });
